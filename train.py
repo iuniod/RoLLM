4a00d283e158f5f63bd2321496875835ecadf6be
Iustina Caramida <iustina.caramida@gmail.com>
2025-03-23 17:15:07 +0200

    change hyperparameters

--- train.py
+++ train.py
@@ -8,16 +8,16 @@ import os
 
 # hyperparameters
 batch_size = 16
-block_size = 32
+block_size = 512
 max_iters = 5000
 eval_interval = 100
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64
-n_head = 4
-n_layer = 4
-dropout = 0.1
+n_embd = 512
+n_head = 12
+n_layer = 8
+dropout = 0.0
 
 torch.manual_seed(1337)
 
