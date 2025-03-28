fdeb795774d824bf4843e9ee1ef574285d7167d6
Iustina Caramida <iustina.caramida@gmail.com>
2025-03-28 14:54:23 +0100

    save the encoded text in a file

--- data_utils.py
+++ data_utils.py
@@ -1,25 +1,33 @@
 import torch
 from tqdm import tqdm
 from transformers import GPT2Tokenizer
+import pandas as pd
 
-def load_data(file_path):
-    cleaned_texts = []
-    with open(file_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            cleaned_texts.append(line.strip())
-    return cleaned_texts
+ENCODED_TEXTS_PATH = 'encoded_texts.pt'
 
 def prepare_data(file_path, model_name="gpt2"):
-    cleaned_texts = load_data(file_path)
+    dp = pd.read_parquet(file_path)
+    texts = dp['text'].tolist()
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-    encoded_texts = [torch.tensor(tokenizer.encode(text), dtype=torch.long) for text in tqdm(cleaned_texts, desc='Encoding texts')]
-    data = torch.cat(encoded_texts)
+    if ENCODED_TEXTS_PATH:
+        try:
+            data = torch.load(ENCODED_TEXTS_PATH)
+        except FileNotFoundError:
+            encoded_texts = [
+                torch.tensor(tokenizer.encode(text, truncation=True, max_length=1024), dtype=torch.long)
+                for text in tqdm(texts, desc='Encoding texts')
+            ]
+            data = torch.cat(encoded_texts)
+
+            torch.save(data, ENCODED_TEXTS_PATH)
 
     return data, tokenizer.vocab_size, tokenizer.encode, tokenizer.decode
+
 
 def split_data(data, train_ratio=0.9):
     n = int(train_ratio * len(data))
     train_data = data[:n]
     val_data = data[n:]
+
     return train_data, val_data
